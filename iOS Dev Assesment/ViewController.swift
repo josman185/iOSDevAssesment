47db34423f188d1672d4c8f20297ff0b15467d1a
josman185 <josman185@hotmail.com>
2024-01-12 12:51:09 -0600

    Initial Commit

---
+++ iOS Dev Assesment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS Dev Assesment
+//
+//  Created by Jose Vargas on 1/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
